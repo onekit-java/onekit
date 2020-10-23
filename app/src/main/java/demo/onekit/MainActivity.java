4feef0bdb929585124965a2da01c2dbe917e21d6
zhangjinva <zhangjin_va@qq.com>
2020-10-23 16:54:40 +0800

    .

--- app/src/main/java/demo/onekit/MainActivity.java
+++ app/src/main/java/demo/onekit/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("=======", OneKit.fixPath("/a/a","../b"));
+        Log.e("======",OneKit.fixPath("/a/index.html","../a/b.html"));
     }
 }
